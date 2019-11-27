23a6995e795a9846de663fbd443ef2a793a10e36
Almaz <almick17933@gmail.com>
2019-11-27 16:07:07 +0300

    Just created class 'Game' without implementation. Added code in ChessPiese and Square

--- src/main/java/ChessPiece.java
+++ src/main/java/ChessPiece.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.List;
 
-public class ChessPiece {
+public abstract class ChessPiece {
+    private final boolean isWhite;
+    private List<Square> availableSquares; // Куда может пойти данная фигура
+
+    public ChessPiece(boolean isWhite) {
+        this.isWhite = isWhite;
+    }
+
+    public boolean isValidMove(Square newPosition) {
+        return (availableSquares.contains(newPosition)) ? true : false;
+    }
+
+    public abstract void refreshValidMoves(Square current); // Сюда ещё должна быть передана доска или массив клеток
 }
--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,2 +1,40 @@
 public class Square {
+    private int x, y;
+    private final boolean isWhite;
+    private ChessPiece currentPiece; // Какая фигура стоит на клетке. Если null - никакая
+
+    public Square(int x, int y, ChessPiece currentPiece) {
+        this.x = x;
+        this.y = y;
+        this.currentPiece = currentPiece;
+        if ((x + y) % 2 == 0) {
+            this.isWhite = false;
+        } else {
+            this.isWhite = true;
+        }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setChessPiece(ChessPiece p) {
+
+    }
+
+    public boolean isWhite() {
+        return isWhite;
+    }
+
+    public ChessPiece getCurrentPiece() {
+        return currentPiece;
+    }
+
+    public boolean isOccupied() {
+        return (currentPiece == null) ? false : true;
+    }
 }
